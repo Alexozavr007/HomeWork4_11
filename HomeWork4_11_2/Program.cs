a69f957c3a5cdf79e930ddc3ade3538da6234524
Alex Kondratiuk <alexozavr007@gmail.com>
2024-08-02 14:44:51 +0300

    Add project files.

---
+++ HomeWork4_11_2/Program.cs
@@ -0,0 +1,47 @@
+﻿
+
+public class ThreadArguments {
+    public string FileToRead { get; set; }
+    public string FileToWrite { get; set; }
+}
+
+public static class Program {
+   static object obj = new object();
+
+    static void WorkWithFile(object args) {
+        var thrArgs = (ThreadArguments)args;
+
+        FileStream file = new FileStream(thrArgs.FileToRead, FileMode.Open, FileAccess.Read);
+        StreamReader reader = new StreamReader(file);
+        string temp = reader.ReadToEnd();
+        reader.Close();
+        file.Close();
+
+        lock (obj) {
+            FileStream file3 = new FileStream(thrArgs.FileToWrite, FileMode.OpenOrCreate, FileAccess.Write);
+            file3.Position = file3.Length;
+            StreamWriter writer = new StreamWriter(file3);
+            writer.Write(temp);
+            writer.Close();
+            file3.Close();
+        }
+    }
+
+
+    public static void Main(string[] args) {
+        string finalFileName = "TextFinal.txt";
+
+        var file1 = new Thread(WorkWithFile);
+        file1.Start(new ThreadArguments{
+            FileToWrite = finalFileName,
+            FileToRead = "TextFile1.txt"
+        });
+        var file2 = new Thread(WorkWithFile);
+        file2.Start(new ThreadArguments {
+            FileToWrite = finalFileName,
+            FileToRead = "TextFile2.txt"
+        });
+        
+    }
+
+}
---
+++ HomeWork4_11_4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+
+// Обмеження: Не можна використовувати об'єкти блокування структурного типу
+// Працюючи з об'єктом класу - Monitor.
+
+// Lock - не працює коректно з об'єктами блокування структурних типів,
+// Допускається використання об'єктів блокування тільки типів посилань.
+
+namespace monitor {
+    class Program {
+
+        static int counter = 0;
+
+        static object block = 0; // block - не повинен бути структурним.
+
+        static void Function() {
+            Console.WriteLine($"Поток стартує {Thread.CurrentThread.GetHashCode()}");
+            for (int i = 0; i < 50; ++i) {
+                // Встановлюється блокування кожен (50!) разів у новий object (boxing).
+                Monitor.Enter((object)block); // boxing створює новий об'єкт (50 об'єктів).
+
+                // Виконання деякої роботи потоком ...
+                Console.WriteLine(++counter);
+
+                // Спроба зняти блокування з об'єкта, який не є об'єктом блокування.
+                Monitor.Exit((object)block); // boxing створює абсолютно новий об'єкт.
+            }
+            Console.WriteLine($"Поток закінчився {Thread.CurrentThread.GetHashCode()}");
+
+        }
+
+        static void Main() {
+            Console.OutputEncoding = Encoding.Unicode;
+            Thread[] threads = { new Thread(Function), new Thread(Function), new Thread(Function) };
+
+            foreach (Thread thread in threads) {
+                thread.Start();
+                thread.Join();
+            }
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
